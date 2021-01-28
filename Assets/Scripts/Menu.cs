d192126a7a857f7d2b2e2cc44d391d4acd602f72
LucasKoulloupias <71901252+LucasKoulloupias@users.noreply.github.com>
2021-01-28 17:23:41 +0000

    Initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit() 
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moving : MonoBehaviour
+{
+    public float speed;
+    public int Score;
+    public Text ScoreBorad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed, 0, 0);
+    }
+
+    public void AddSpeed()
+    {
+        speed = speed + 0.01f;
+
+        Score = Score + 10;
+
+        ScoreBorad.text = Score.ToString();
+    }
+}
